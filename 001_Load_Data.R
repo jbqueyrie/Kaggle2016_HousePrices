f35b05ef232e771152e11d53aaf12281f2ab539d
jbqueyrie <jbqueyrie@gmail.com>
2016-10-18 16:43:04 +0200

    Added various functions to plot data

--- 001_Load_Data.R
+++ 001_Load_Data.R
@@ -41,6 +41,8 @@ test[,1] = as.factor(test[,1])
 str(train)
 str(test)
 
+dt.out = dt[, list(v1 = sum(v1),  lapply(.SD,mean)), by = grp, .SDcols = sd.cols]
+
 # Check missing values: global / cat_var / num_var
 colSums(sapply(train, is.na))
 colSums(sapply(train[,.SD,.SDcols = cat_var], is.na))
@@ -57,4 +59,34 @@ par(mfrow=c(1,2))
 hist(log(train$SalePrice),xlab="Log sale price",main="Log sale prices distribution",col="red")
 qqnorm(log(train$SalePrice), main="Normal QQ-plot for log(SalePrice)")
 qqline(log(train$SalePrice))
+
+
+# Plot lots of variables
+train_cat <- train[,.SD, .SDcols = cat_var]
+train_cont <- train[,.SD,.SDcols = num_var]
+
+plotHist <- function(data_in, i) {
+  data <- data.frame(x=data_in[[i]])
+  p <- ggplot(data=data, aes(x=factor(x))) + stat_count() + xlab(colnames(data_in)[i]) + theme_light() + 
+    theme(axis.text.x = element_text(angle = 90, hjust =1))
+  return (p)
+}
+
+doPlots <- function(data_in, fun, ii, ncol=3) {
+  pp <- list()
+  for (i in ii) {
+    p <- fun(data_in=data_in, i=i)
+    pp <- c(pp, list(p))
+  }
+  do.call("grid.arrange", c(pp, ncol=ncol))
+}
+
+plotDen <- function(data_in, i){
+  data <- data.frame(x=data_in[[i]], SalePrice = data_in$SalePrice)
+  p <- ggplot(data= data) + geom_line(aes(x = x), stat = 'density', size = 1,alpha = 1.0) +
+    xlab(paste0((colnames(data_in)[i]), '\n', 'Skewness: ',round(skewness(data_in[[i]], na.rm = TRUE), 2))) + theme_light() 
+  return(p)
+}
+
+
 

3fc6a5983ce5a1dae61f7bd7187c5bfce48be07d
jbqueyrie <jbqueyrie@gmail.com>
2016-11-03 15:05:41 +0100

    Plots of factors and numeric

--- RScripts/001_Load_Data.R
+++ RScripts/001_Load_Data.R
@@ -20,7 +20,7 @@ library(corrplot)
 library(GGally)
 library(ggplot2)
 library(e1071)
-library(dplyr)
+library(dtplyr)
 
 # DF exploration
 names(train)
@@ -29,16 +29,9 @@ str(train)
 
 dim(train)
 
-# Categorical / numeric variables
-cat_var <- names(train)[which(sapply(train, is.character))]
-cat_car <- c(cat_var, 'BedroomAbvGr', 'HalfBath', ' KitchenAbvGr','BsmtFullBath', 'BsmtHalfBath', 'MSSubClass')
-num_var <- names(train)[which(sapply(train, is.numeric))]
-
-
 # Convert ID to character
 train[,1] = as.character(train[,1])
 test[,1] = as.character(test[,1])
-
 
 # Convert integer variables to factors
 names=c('MSSubClass','OverallQual','OverallCond')
@@ -48,11 +41,22 @@ test[,names] <- lapply(test[,names] , factor)
 str(train)
 str(test)
 
-dt.out = dt[, list(v1 = sum(v1),  lapply(.SD,mean)), by = grp, .SDcols = sd.cols]
+# Categorical / numeric variables
+num_var <- names(train)[which(sapply(train, is.numeric))]
+cat_var <- names(train)[which(sapply(train, is.factor))]
 
 
 # Summary statistics for each variable
 summary(train)
+
+
+
+# Check missing values
+sapply(train, function(x) sum(is.na(x)))
+sapply(train, function(x) round(sum(is.na(x))/nrow(train),2))
+
+missing_var = colnames(train)[apply(train, 2, anyNA)]
+sapply(train[,missing_var], function(x) round(sum(is.na(x))/nrow(train),2))
 
 
 # Check log(SalePrice) - variable to explain
@@ -69,14 +73,21 @@ shapiro.test(log(train$SalePrice)) # -> Normality does not hold, p-value < 0.05 
 
 
 # Plot lots of variables
-train_cat <- train[,.SD, .SDcols = cat_var]
-train_cont <- train[,.SD,.SDcols = num_var]
+train_cat <- train[,cat_var]
+train_cont <- train[,num_var]
 
 plotHist <- function(data_in, i) {
   data <- data.frame(x=data_in[[i]])
   p <- ggplot(data=data, aes(x=factor(x))) + stat_count() + xlab(colnames(data_in)[i]) + theme_light() + 
     theme(axis.text.x = element_text(angle = 90, hjust =1))
   return (p)
+}
+
+plotDen <- function(data_in, i){
+  data <- data.frame(x=data_in[[i]], SalePrice = data_in$SalePrice)
+  p <- ggplot(data= data) + geom_line(aes(x = x), stat = 'density', size = 1,alpha = 1.0) +
+    xlab(paste0((colnames(data_in)[i]), '\n', 'Skewness: ',round(skewness(data_in[[i]], na.rm = TRUE), 2))) + theme_light() 
+  return(p)
 }
 
 doPlots <- function(data_in, fun, ii, ncol=3) {
@@ -88,15 +99,23 @@ doPlots <- function(data_in, fun, ii, ncol=3) {
   do.call("grid.arrange", c(pp, ncol=ncol))
 }
 
-plotDen <- function(data_in, i){
-  data <- data.frame(x=data_in[[i]], SalePrice = data_in$SalePrice)
-  p <- ggplot(data= data) + geom_line(aes(x = x), stat = 'density', size = 1,alpha = 1.0) +
-    xlab(paste0((colnames(data_in)[i]), '\n', 'Skewness: ',round(skewness(data_in[[i]], na.rm = TRUE), 2))) + theme_light() 
-  return(p)
+
+# Plots of cat_var (4 graphs per window)
+for (i in seq(1,ncol(train_cat),4)) {
+  if ((i+3) <= ncol(train_cat)) {
+    doPlots(train_cat, fun = plotHist, ii = i:(i+3), ncol = 2)
+  }
 }
 
+names(train_cont)
+# Plots of num_var (4 graphs per window)
+doPlots(train_cont, fun = plotDen, ii = 1:6, ncol = 2)
+doPlots(train_cont, fun = plotDen, ii = 7:13, ncol = 2)
+doPlots(train_cont, fun = plotHist, ii = 14:18, ncol = 2)
+doPlots(train_cont, fun = plotHist, ii = 19:20, ncol = 2)
+doPlots(train_cont, fun = plotHist, ii = 19:20, ncol = 2)
+doPlots(train_cont, fun = plotDen, ii = 28:34, ncol = 2)
+doPlots(train_cont, fun = plotHist, ii = 33, ncol = 1)
 
-# Check missing values: global / cat_var / num_var
-colSums(sapply(train, is.na))
-colSums(sapply(train[,.SD,.SDcols = cat_var], is.na))
-colSums(sapply(train[,.SD,.SDcols = num_var], is.na))
+
+
